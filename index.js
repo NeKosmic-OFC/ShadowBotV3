c2a6288ecacf6a8905fe9c4553d56748fee6130e
Alpha-OFC <98774175+Alpha-OFC@users.noreply.github.com>
2022-02-15 10:23:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ CFonts.say('The Shadow\nBrokers\nBot', {
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`Subscribete a The Shadow Brokers - Bot\n\nBot creado por Bruno Sobrino`, {
+CFonts.say(`Subscribete a AnimeEdits_4k - Bot\n\nBot creado por ¥Alpha_ØFC¥`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
