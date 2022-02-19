0f65a32528104172c25d343492ce95d04eaa1a08
Alpha-OFC <98774175+Alpha-OFC@users.noreply.github.com>
2022-02-19 12:22:42 +0000

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,8 +1,8 @@
 function handler(m) {
   this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-  this.sendContact(m.chat, '14502335923', 'SHADOW N1 - CREADOR - OFICIAL', m)
-  this.sendContact(m.chat, '17722386341', 'SHADOW N2 - CREADOR - OFICIAL', m)
-  this.sendContact(m.chat, '14503280343', 'SHADOW N3 - CREADOR - OFICIAL', m)
+  this.sendContact(m.chat, '34623442554', 'NEKOTINE N1 - CREADOR - OFICIAL', m)
+  this.sendContact(m.chat, '34623442554', 'NEKOTINE N2 - CREADOR - OFICIAL', m)
+  this.sendContact(m.chat, '34623442554', 'NEKOTINE N3 - CREADOR - OFICIAL', m)
   }
 handler.help = ['contacto']
 handler.tags = ['info']
