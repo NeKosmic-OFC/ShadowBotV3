a0fab9e30fb5c5c3fce88e6ae066c7dccf452230
Alpha-OFC <98774175+Alpha-OFC@users.noreply.github.com>
2022-02-19 12:20:37 +0000

    Update bot.shadow.vos.js

--- plugins/bot.shadow.vos.js
+++ plugins/bot.shadow.vos.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /the shadow brokers - bot|The Shadow Brokers - Bot|tsbb|TSBB|Shadow Brokers - Bot|shadow brokers - bot|Shadow bot|shadow bot|theshadowbrokersbot|Shadow Bot|Hola Bot|hola bot|Hola bot|hola bot|ShadowBot|shadowbot/
+handler.customPrefix = /NeKotine - bot|NeKotine - Bot|tsbb|TSBB|NeKotine - Bot|NeKotine - bot|NeKotine bot|NeKotine bot|NeKotineBot|NeKotine Bot|Hola Bot|hola bot|Hola bot|hola bot|NeKotine|NeKotineBot/
 handler.command = new RegExp
 module.exports = handler
