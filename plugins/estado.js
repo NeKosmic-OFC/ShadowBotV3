bab9db88b9774d0f8950dce0f25b0047fbf51496
Alpha-OFC <98774175+Alpha-OFC@users.noreply.github.com>
2022-02-19 12:41:24 +0000

    Update estado.js

--- plugins/estado.js
+++ plugins/estado.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 *ミ🤖 Estado del Bot 🤖彡*
 *=> Bot activo ✔️*
 *=> Bot uso público ✔️*
-`.trim(), '©The Shadow Borkers - Bot', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`)
+`.trim(), '©NeKotine - Bot', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`)
 }
 handler.command = /^(estado|status|estate|state|stado|stats)$/i
 
