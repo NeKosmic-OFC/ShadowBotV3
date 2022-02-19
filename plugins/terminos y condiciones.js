9f5b1dd7fd1f68a10b89596d02f0cd9d12ba372d
Alpha-OFC <98774175+Alpha-OFC@users.noreply.github.com>
2022-02-19 13:25:47 +0000

    Update terminos y condiciones.js

--- plugins/terminos y condiciones.js
+++ plugins/terminos y condiciones.js
@@ -20,7 +20,7 @@ _2.- No nos hacemos responsables de la ignorancia y/o desconocimiento del tema_
 _3.- El Bot no esta activo las 24 horas al menos de que el propietario decida lo contrario_
 _4.- La "empresa" no se hace responsable del uso de un Sub Bot o Bot no oficial, ya que son personas (terceros) y no tenemos control sobre ell@s_
 _5.- No nos hacemos responsables de los números que se puedan ir a soporte por el uso del Bot, es aconsejable siempre usar números virtuales para usos de Bots_
-_6.- Los audios/notas de voz/imágenes/videos o cualquier otro archivo multimedia que sean de propiedad de The Shadow Brokers - Bot son únicamente y exclusivamente de este Bot, si se detecta a otro Bot con alguno o algunos de los archivos multimedia antes mencionado se tomarán cartas en el asunto._
+_6.- Los audios/notas de voz/imágenes/videos o cualquier otro archivo multimedia que sean de propiedad de NeKotine - Bot son únicamente y exclusivamente de este Bot, si se detecta a otro Bot con alguno o algunos de los archivos multimedia antes mencionado se tomarán cartas en el asunto._
 
 *➤ Mensaje del numero del Bot?*
 
@@ -29,13 +29,13 @@ _- Si en algún momento recibe un mensaje del numero del Bot y no es un comando 
 *⁉️ Te quedaron dudas?*
 
 _- Si todavía tienes alguna duda o reclamo de lo mencionado aqui o referente al Bot, contáctame a mi numero personal al cual estaré respondiendo lo antes posible para brindarte una solución y/o hacerme responsable de alguna reclamación_ 
-> wa.me/5219996125657
-> Bruno Sobrino 
-> Correo electronico: theshadowbrokers133@gmailcom
+> wa.me/+34623442554
+> ¥Alpha_ØFC¥
+> Correo electronico: nekotine.bot@gmail.com 
 
-→ *Solo contactar por temas serios, para preguntas del Bot mas generales contáctame al privado con el comando del comando #owner (al numero wa.me/17722386341)*
+→ *Solo contactar por temas serios, para preguntas del Bot mas generales contáctame al privado con el comando del comando #owner (al numero wa.me/+34623442554)*
 
-*∆ THE SHADOW BROKERS - BOT ∆*
+*∆ NeKotine - BOT ∆*
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
