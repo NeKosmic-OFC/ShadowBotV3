6b8ae2200eec11519b7586fd541f9f58c546d6c1
Alpha-OFC <98774175+Alpha-OFC@users.noreply.github.com>
2022-02-21 19:00:05 +0000

    Update donar.js

--- plugins/donar.js
+++ plugins/donar.js
@@ -7,7 +7,8 @@ let handler = async m => m.reply(`
 *┃ ❖ Hola persona hermosa ❤️*
 *┃ 👉 Aquí tienes algunos datos*
 *┃ para que puedas apoyar <3*
-*┃  - Mis redes: https://vm.tiktok.com/ZMLNSV3YV/
+*┃  - Mis redes: 👇
+*┃  - https://vm.tiktok.com/ZMLNSV3YV/
 *┃  - Se agradecen tus donaciónes*
 *┃  - CONCEPTO: Donar*
 *┃  - BENEFICIARIO: ¥Alpha_ØFC¥ Creador de NeKotine-BOT*
