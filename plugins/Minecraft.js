4de38fd244b4bdd6f4fa9d8e191acde919375ea2
Alpha-OFC <98774175+Alpha-OFC@users.noreply.github.com>
2022-02-25 18:24:37 +0000

    Update Minecraft.js

--- plugins/Minecraft.js
+++ plugins/Minecraft.js
@@ -15,3 +15,11 @@ let handler = async (m, { conn, usedPrefix }) => {
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
     let str = `
+*_⎈𝐒𝐞𝐫𝐯𝐞𝐫 𝐝𝐞 𝐌𝐢𝐧𝐞𝐜𝐫𝐚𝐟𝐭⎈
+ ꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷
+║➤𝐍𝐨𝐦𝐛𝐫𝐞: 𝐿𝑒𝑔𝑖𝑜𝑛 𝑂𝑡𝑎𝑘𝑢
+║➤𝐈𝐏:Legiónotaku12.𝐩𝐚𝐫𝐚 𝐬𝐞𝐠𝐮𝐢𝐫 𝐜𝐨𝐧𝐭𝐚𝐜𝐭𝐚 𝐜𝐨𝐧 𝐞𝐥 𝐝𝐮𝐞𝐧̃𝐨 𝐝𝐞𝐥 𝐬𝐞𝐫𝐯𝐞𝐫
+║➤𝐏𝐮𝐞𝐫𝐭𝐨: 49804
+║➤𝐕𝐞𝐫𝐬𝐢𝐨𝐧: 1.18.12
+║➤𝐅𝐮𝐧𝐜𝐢𝐨𝐧𝐚 𝐮𝐬𝐚𝐧𝐝𝐨 𝐚𝐩𝐤.
+║➤𝐍𝐮𝐦𝐞𝐫𝐨 𝐚 𝐜𝐨𝐧𝐭𝐚𝐜𝐭𝐚𝐫 𝐩𝐚𝐫𝐚 𝐮𝐧𝐢𝐫𝐭𝐞 𝐚𝐥 𝐬𝐞𝐫𝐯𝐞𝐫 wa.me//+1(561)618-0386
